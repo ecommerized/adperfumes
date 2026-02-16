eccc7a3c90b2a300bd8873a452e61e4900033a1a
ecommerized <info@ecommerized.com>
2026-02-16 11:48:22 +0400

    Make notifications synchronous instead of queued for immediate delivery

--- app/Notifications/OrderStatusChanged.php
+++ app/Notifications/OrderStatusChanged.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class OrderStatusChanged extends Notification implements ShouldQueue
+class OrderStatusChanged extends Notification // implements ShouldQueue
 {
     use Queueable;
 
